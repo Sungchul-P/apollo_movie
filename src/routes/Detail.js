2eaa08d6b83c02dc0d389bf73bdab95739bbf331
sungchul-dev <devnori@bigin.io>
2020-03-21 21:59:53 +0900

    1.1 GET_MOVIES Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,30 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Detail</h1>;
+const GET_MOVIES = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  let { id } = useParams();
+  id = parseInt(id);
+  const { loading, data } = useQuery(GET_MOVIES, {
+    variables: { id }
+  });
+  console.log(loading, data);
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
